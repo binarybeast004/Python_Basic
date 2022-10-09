06e2c6b3e0eabbbaa6a25d11475f91570314ffa8
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2022-10-09 18:34:02 +0600

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,14 @@
+import math
+
+
+from math import*
+
+print("\"sadi\"")
+#my first program
+'''
+My age is , age , year .
++name is to connect to the string 
+'''
+
+name =input("Enter your name : ")
+age=input("Enter your age : ")
